f53da5d5a065dc7c5d556d9ca8272d2c85c5d10d
hadidy100 <hadidy100@gmail.com>
2017-09-24 16:36:16 -0700

    amflm lmklmkawdl
    :!
    q!

--- parseLanguageWords2.cpp
+++ parseLanguageWords2.cpp
@@ -30,34 +30,34 @@ Language = a* (a+b) c* is NDFA so we convert it to DFA as bellow based on the fo
                         State {S,b}
 
     state {s}             /---\
-	 /---\	             /     \
-	/     \              |  +  |======>> b or c going to {b} >>======>>
+     /---\               /     \
+    /     \              |  +  |======>> b or c going to {b} >>======>>
     |  -  |===== a ====>>|     |                                       )
-	|     |              \     /                                      )
+    |     |              \     /                                      )
     \     /               \___/                                      )
-	 \___/\\                ()                                      )
-	       \\                a  loop                               )
-		    \\                                                    )
-			 \\                                                  )
-			  \\                                                )
-			   \\                                              )
-			     b                                            )
-				 \\                                          )
-				  \\                                        )
-				   \\                                      )
-				    \\                                    )
-					 VV                                  )
-				   /---\	                            )
-				  /     \===>()c loop                  )
-				  |  +  |                             )
-				  |	    |                            /
-				  \	    /<<====== b or c <<=========/
-				   \---/
-				 
-				 State{b}
-				 
-				 
-
+     \___/\\                ()                                      )
+           \\                a  loop                               )
+            \\                                                    )
+             \\                                                  )
+              \\                                                )
+               \\                                              )
+                 b                                            )
+                 \\                                          )
+                  \\                                        )
+                   \\                                      )
+                    \\                                    )
+                     VV                                  )
+                    /---\                               )
+                   /     \===>()c loop                 )
+                   |  +  |                            )
+                   |     |                           /
+                   \     /<<====== b or c <<========/
+                    \---/
+                           
+                   State{b}      
+                                 
+                                 
+                                 
 */
 #include <iostream>
 #include <string> 
@@ -66,105 +66,105 @@ Language = a* (a+b) c* is NDFA so we convert it to DFA as bellow based on the fo
 //***********************prototypes 
 bool validateStateB  (const char&);
 bool validateStateSB (const std::string&, int&);
-bool stringIsValid 	 (const std::string&);	
+bool stringIsValid   (const std::string&); 
 std::string removeDollarSignFromString(std::string&);
 //********************************************
 bool stringIsValid (const std::string& theInitialString)
 {
-	int theSize = theInitialString.size(); 
-	return (theInitialString[theSize-1] == '$'); 
+ int theSize = theInitialString.size(); 
+ return (theInitialString[theSize-1] == '$'); 
 }
 //*****************************************************************************************
 std::string removeDollarSignFromString(std::string& theInitialString)
 {
-	std::string  newString = "";
-	for (int cursor =0; cursor < theInitialString.size()-1; cursor++) 
-	{
-		newString = newString + theInitialString[cursor];
-	}
-	return newString; 
+ std::string  newString = "";
+ for (int cursor =0; cursor < theInitialString.size()-1; cursor++) 
+ {
+  newString = newString + theInitialString[cursor];
+ }
+ return newString; 
 }
 //*****************************************************************************************
 bool validateStateB(const char& theCharacter)
 {
-		return (theCharacter == 'c');
+  return (theCharacter == 'c');
 }
 //********************************************
 bool validateStateSB(const std::string& lexeme, int& cursor)
 {
-	 /*
-	 if lexeme[0] = 'b' 
-	 {
-		 return validateStateB(lexeme[cursor]);
-	 }
-	 */
-	 //else if lexeme[0] = 'a' 
-	 //{
-		 if (lexeme[cursor] == 'a') 
-		 {
-			 return true; 
-		 }
-		 else if (lexeme[cursor] == 'b' || lexeme[cursor] == 'c') 
-		 {
-			if ( cursor+1 < lexeme.size() ) 
-			{
-				return validateStateB(lexeme[cursor+1]); // manipulate the status of stateB within stateSB
-			}
-			else 
-			{
-				return validateStateB(lexeme[cursor]); // manipulate the status of stateB within stateSB
-			}
-		 }
-		 else 
-			 return false;
-	// }
-	    
+  /*
+  if lexeme[0] = 'b' 
+  {
+   return validateStateB(lexeme[cursor]);
+  }
+  */
+  //else if lexeme[0] = 'a' 
+  //{
+   if (lexeme[cursor] == 'a') 
+   {
+    return true; 
+   }
+   else if (lexeme[cursor] == 'b' || lexeme[cursor] == 'c') 
+   {
+   if ( cursor+1 < lexeme.size() ) 
+   {
+    return validateStateB(lexeme[cursor+1]); // manipulate the status of stateB within stateSB
+   }
+   else 
+   {
+    return validateStateB(lexeme[cursor]); // manipulate the status of stateB within stateSB
+   }
+   }
+   else 
+    return false;
+ // }
+     
 }
 //***********************************************
 int main()
 {
-		bool 				stateS  = false; 
-		bool 				stateB  = true; 
-		bool 				stateSB = true;
-		std::string 		lexeme  = "";
-		int 				cursor  = 0;
+  bool     stateS  = false; 
+  bool     stateB  = true; 
+  bool     stateSB = true;
+  std::string   lexeme  = "";
+  int     cursor  = 0;
 //*************************************************
-		std::cout << "Enter a word to be parse: " ; 
-		std::getline (std::cin, lexeme); 
-		if (stringIsValid(lexeme))
-		{
-			lexeme = removeDollarSignFromString(lexeme);		
-			(lexeme[0] == 'a' || lexeme[0] == 'b') ? stateS = true : stateS = false;
-			while (stateS && stateB && stateSB && cursor < lexeme.size())
-			{
-				cursor++; 
-				if (lexeme[0] == 'b')
-				{
-					stateB = validateStateB( lexeme[cursor] );
-				}
-				else if (lexeme[0] == 'a')
-				{
-					validateStateSB (lexeme,cursor); 
-				}
-				else 
-				{
-					stateS = false; stateB = false; stateSB = false;  // not necesssary but why not 
-				}
-				
-			}// end while loop 
-			
-			if (stateS && stateB && stateSB) 
-			{
-				std::cout << "The word: " << lexeme << " that you entered is accepted! " << std::endl;
-			}
-			else 
-			{
-				std::cout << "The word: " << lexeme << " that you entered is not accepted! " << std::endl;
-			}
-		}
-		else // if string is not valid 
-		{ 
-			std::cout << "The string that you entered is not valid, you need to end it with a dollar sign" << std::endl;
-		}
+  std::cout << "Enter a word to be parse: " ; 
+  std::getline (std::cin, lexeme); 
+  if (stringIsValid(lexeme))
+  {
+   lexeme = removeDollarSignFromString(lexeme);  
+   (lexeme[0] == 'a' || lexeme[0] == 'b') ? stateS = true : stateS = false;
+   while (stateS && stateB && stateSB && cursor < lexeme.size())
+   {
+    cursor++; 
+    if (lexeme[0] == 'b')
+    {
+     stateB = validateStateB( lexeme[cursor] );
+    }
+    else if (lexeme[0] == 'a')
+    {
+     validateStateSB (lexeme,cursor); 
+    }
+    else 
+    {
+     stateS = false; stateB = false; stateSB = false;  // not necesssary but why not 
+    }
+    
+   }// end while loop 
+   
+   if (stateS && stateB && stateSB) 
+   {
+    std::cout << "The word: " << lexeme << " that you entered is accepted! " << std::endl;
+   }
+   else 
+   {
+    std::cout << "The word: " << lexeme << " that you entered is not accepted! " << std::endl;
+   }
+  }
+  else // if string is not valid 
+  { 
+   std::cout << "The string that you entered is not valid, you need to end it with a dollar sign" << std::endl;
+  }
 
 }
